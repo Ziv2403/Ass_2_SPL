80e56a7eb1c34418224a79549fb76b81b11d5ced
yanivmihai <yaniv4567@gmail.com>
2024-12-29 17:23:53 +0200

    2

--- src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
@@ -1,5 +1,11 @@
 package bgu.spl.mics.application.messages;
 
+import bgu.spl.mics.Event;
+import bgu.spl.mics.application.objects.DetectedObject;
+import bgu.spl.mics.application.objects.StampedDetectedObjects;
+
+import java.util.ArrayList;
+import java.util.List;
 /**
  * DetectObjectsEvent
  * •
@@ -18,6 +24,25 @@ package bgu.spl.mics.application.messages;
  * o
  * After the LiDar Worker completes the event, it saves the coordinates in the lastObjects variable in DataBase and sends True value to the Camera.
  */
-public class DetectObjectsEvent {
+public class DetectObjectsEvent implements Event<List<StampedDetectedObjects>>{
 
+// --------------------- fields --------------------
+
+    private final StampedDetectedObjects detectedObjects ;
+    private final int cameraId;
+    private final String description;
+
+    // --------------------- constructor --------------------
+    public DetectObjectsEvent(StampedDetectedObjects detectedObjects, int cameraId,String description){
+        this.detectedObjects = detectedObjects;
+        this.cameraId = cameraId;
+        this.description = description;
+    }
+
+    // --------------------- Methods --------------------
+//Getters
+    public StampedDetectedObjects getDetectedObjects() {return detectedObjects;}
+    public int getCameraId() {return cameraId;}
+    public String getDescription() {return description;}
 }
+
